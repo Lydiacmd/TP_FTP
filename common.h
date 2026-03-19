4c599263e58d5771d40e78e0a3b32f564ceccaf4
Lydiacmd <belab05bas@gmail.com>
2026-03-19 08:43:38 +0100

    Q1-Q2 : typereq_t, request_t

---
+++ common.h
@@ -0,0 +1,19 @@
+#ifndef COMMON_H
+#define COMMON_H
+#define MAX_FILENAME 256
+
+
+typedef enum {
+    GET = 0,
+    PUT = 1,
+    LS = 2
+} typereq_t;
+
+typedef struct {
+    typereq_t type;
+    char nom_Fichier[MAX_FILENAME];
+} request_t;
+
+
+
+#endif
